0df95fbbf501c3ce4621dbcaf94fe2b87945ad24
elinapuman <elina.puman@khk.ee>
2013-10-06 13:58:03 +0300

    Renew database dump

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2013 at 10:41 PM
+-- Generation Time: Oct 06, 2013 at 12:52 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
